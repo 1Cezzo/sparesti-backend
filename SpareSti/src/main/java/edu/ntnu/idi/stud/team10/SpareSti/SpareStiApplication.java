7a33b91ce99d1e8f599dad44da6fb19870afc650
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-15 10:26:26 +0200

    Add empty template spring boot project

---
+++ SpareSti/src/main/java/edu/ntnu/idi/stud/team10/SpareSti/SpareStiApplication.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.stud.team10.SpareSti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpareStiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpareStiApplication.class, args);
+	}
+
+}
---
+++ SpareSti/src/test/java/edu/ntnu/idi/stud/team10/SpareSti/SpareStiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ntnu.idi.stud.team10.SpareSti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpareStiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
