1643ca0d2fbb00035da3bece9298f8007a88c5f2
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-16 13:22:21 +0200

    Implement basic Badge and BadgeDto functionality

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/dto/BadgeDto.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/dto/BadgeDto.java
@@ -1,8 +1,26 @@
 package edu.ntnu.idi.stud.team10.sparesti.dto;
 
+import edu.ntnu.idi.stud.team10.sparesti.model.Badge;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class BadgeDto {
     private long id;
     private String title;
     private String description;
     private String imageUrl;
+
+    /**
+     *
+     * @param badge (Badge) The badge being converted
+     */
+    public BadgeDto(Badge badge) {
+        this.id = badge.getId();
+        this.title = badge.getTitle();
+        this.description = badge.getDescription();
+        this.imageUrl = badge.getImageUrl();
+    }
 }
+
--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/model/Badge.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/model/Badge.java
@@ -1,8 +1,30 @@
 package edu.ntnu.idi.stud.team10.sparesti.model;
 
+
+import edu.ntnu.idi.stud.team10.sparesti.dto.BadgeDto;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+/**
+ * Badge entity for storage in database
+ */
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "badges")
 public class Badge {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String title;
+
+    @Column(name = "name", nullable = false)
+    private String title; //this and/or imageurl could theoretically be unique?
+
+    @Column(name = "description", nullable = false)
     private String description;
+
+    @Column(name = "image_url", nullable = false)
     private String imageUrl;
 }
