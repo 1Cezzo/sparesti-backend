6a266ca1f30424d697877bdfb08199b115d92f7e
Emil Johnsen <emij@stud.ntnu.no>
2024-04-16 10:32:29 +0200

    Revert "Merge branch '12-automatic-code-formatting+ci' into 'main'"
    This reverts merge request !4

--- SpareSti/src/main/java/edu/ntnu/idi/stud/team10/SpareSti/SpareStiApplication.java
+++ SpareSti/src/main/java/edu/ntnu/idi/stud/team10/SpareSti/SpareStiApplication.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idi.stud.team10.sparesti;
+package edu.ntnu.idi.stud.team10.SpareSti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpareStiApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(SpareStiApplication.class, args);
-  }
+	public static void main(String[] args) {
+		SpringApplication.run(SpareStiApplication.class, args);
+	}
+
 }
--- SpareSti/src/test/java/edu/ntnu/idi/stud/team10/SpareSti/SpareStiApplicationTests.java
+++ SpareSti/src/test/java/edu/ntnu/idi/stud/team10/SpareSti/SpareStiApplicationTests.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idi.stud.team10.sparesti;
+package edu.ntnu.idi.stud.team10.SpareSti;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpareStiApplicationTests {
 
-  @Test
-  void contextLoads() {}
+	@Test
+	void contextLoads() {
+	}
+
 }
