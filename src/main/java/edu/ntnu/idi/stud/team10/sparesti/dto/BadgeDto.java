ce5b6809847becc989ff471ad60031d002705f43
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-16 12:18:34 +0200

    Make simple badge class

---
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/dto/BadgeDto.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.stud.team10.sparesti.dto;
+
+public class BadgeDto {
+    private long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+}
---
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/model/Badge.java
@@ -0,0 +1,8 @@
+package edu.ntnu.idi.stud.team10.sparesti.model;
+
+public class Badge {
+    private long id;
+    private String title;
+    private String description;
+    private String imageUrl;
+}
