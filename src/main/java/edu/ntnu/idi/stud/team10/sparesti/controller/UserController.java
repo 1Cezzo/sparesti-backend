9ce577870210bc60199357bdab0879a4a0bb7651
psued <psuedoari@gmail.com>
2024-04-16 11:15:13 +0200

    Addition(UserController.java): Added a sign up endpoint
    Added the UserController.java class which allows for signing in and deleting a user

---
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/controller/UserController.java
@@ -0,0 +1,57 @@
+package edu.ntnu.idi.stud.team10.sparesti.controller;
+
+import edu.ntnu.idi.stud.team10.sparesti.dto.UserDto;
+import edu.ntnu.idi.stud.team10.sparesti.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for handling user requests.
+ */
+@RestController
+@RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * Get a user by username.
+     *
+     * @param username the username of the user
+     * @return the user with the given username
+     */
+    @GetMapping("/{username}")
+    public ResponseEntity<UserDto> getUserByUsername(@PathVariable String username) {
+        return userService.getUserByUsername(username);
+    }
+
+    /**
+     * Create a new user.
+     *
+     * @param userDTO the user to create
+     * @return the response entity
+     */
+    @PostMapping("/create")
+    public ResponseEntity<String> createUser(@RequestBody UserDto userDTO) {
+        return userService.addUser(userDTO);
+    }
+
+    @DeleteMapping("/delete/{username}")
+    public ResponseEntity<String> deleteUser(@PathVariable String username) {
+        return userService.deleteUser(username);
+    }
+}
