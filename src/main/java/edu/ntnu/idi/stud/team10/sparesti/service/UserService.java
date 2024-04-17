17697d3d07100f05f9092df686e8370cfec3b84c
1Cezzo <emiljohnzen@gmail.com>
2024-04-17 17:23:59 +0200

    fixed merge conflict????

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/service/UserService.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/service/UserService.java
@@ -5,11 +5,10 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import edu.ntnu.idi.stud.team10.sparesti.dto.SavingsGoalDTO;
@@ -148,7 +147,6 @@ public class UserService {
         .orElseThrow(() -> new InvalidIdException("User with username " + username + " not found"));
   }
 
-  @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
     User foundUser = findUserByUsername(username);
     return org.springframework.security.core.userdetails.User.builder()
