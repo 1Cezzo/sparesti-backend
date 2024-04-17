8b33d210ab498b4649e514236c12a56cb890f2ae
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-17 10:10:12 +0200

    fix: Use mvn spotless:apply
    To ensure pipeline on merge succeeds

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/dto/BadgeDto.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/dto/BadgeDto.java
@@ -3,34 +3,30 @@ package edu.ntnu.idi.stud.team10.sparesti.dto;
 import edu.ntnu.idi.stud.team10.sparesti.model.Badge;
 import lombok.*;
 
-/**
- * Data transfer object for Badge entities
- */
+/** Data transfer object for Badge entities */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class BadgeDto {
-    private long id;
-    private String title;
-    private String description;
-    private String imageUrl;
+  private long id;
+  private String title;
+  private String description;
+  private String imageUrl;
 
-    /**
-     * Converts badge into DTO for data transfer.
-     *
-     * @param badge (Badge) The badge being converted
-     */
-    public BadgeDto(Badge badge) {
-        this.id = badge.getId();
-        this.title = badge.getTitle();
-        this.description = badge.getDescription();
-        this.imageUrl = badge.getImageUrl();
-    }
+  /**
+   * Converts badge into DTO for data transfer.
+   *
+   * @param badge (Badge) The badge being converted
+   */
+  public BadgeDto(Badge badge) {
+    this.id = badge.getId();
+    this.title = badge.getTitle();
+    this.description = badge.getDescription();
+    this.imageUrl = badge.getImageUrl();
+  }
 
-    /** */
-    public Badge toEntity() {
-        return new Badge(id, title, description, imageUrl);
-    }
-
+  /** */
+  public Badge toEntity() {
+    return new Badge(id, title, description, imageUrl);
+  }
 }
-
--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/repository/BadgeRepository.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/repository/BadgeRepository.java
@@ -1,7 +1,8 @@
 package edu.ntnu.idi.stud.team10.sparesti.repository;
 
-import edu.ntnu.idi.stud.team10.sparesti.model.Badge;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.ntnu.idi.stud.team10.sparesti.model.Badge;
 
 /** Repository for Badge entities. */
 public interface BadgeRepository extends JpaRepository<Badge, Long> {}
