f76f71ed33f5d3cf62ebd2503a677290c1783194
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-18 10:40:39 +0200

    docs: Add todo concerning displaying a user's full name on their profile
    "TODO: add full name here? (might come from the mock bank?) Designer Sondre claimed that since we are connecting to the bank, the user's name should be visible on profile page"

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/model/User.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/model/User.java
@@ -23,7 +23,7 @@ public class User {
 
   @Column(unique = true)
   private String username;
-
+  //TODO: add full name here? (might come from the mock bank?) Designer(s) claimed that since we are connecting to the bank, the user's name should be visible on profile page
   private String password;
   private String email;
   private String profilePictureUrl;
