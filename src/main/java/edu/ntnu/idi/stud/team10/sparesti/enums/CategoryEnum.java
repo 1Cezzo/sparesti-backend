dbbf1a676d399bc795f10bed175b8f93b65feae2
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-22 12:41:10 +0200

    Addition: Add "NONE" enum to categories
    This is intended for transactions where a user is transferring money between their own accounts. It is excluded from random mock transactions.

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/enums/CategoryEnum.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/enums/CategoryEnum.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
 
 /** Enum representing the different categories that a budget row or transaction can have. */
@@ -13,13 +14,25 @@ public enum CategoryEnum {
   ENTERTAINMENT,
   CLOTHING,
   UTILITIES,
-  OTHER;
+  OTHER,
+  NONE; //for when a user is transferring between their checkings- and savings accounts
 
+  // Filter out the NONE category
   private static final List<CategoryEnum> VALUES =
-          List.of(values());
+          Collections.unmodifiableList(
+                  Arrays.stream(values())
+                          .filter(category -> category != NONE)
+                          .collect(Collectors.toList())
+          );
   private static final int SIZE = VALUES.size();
   private static final Random RANDOM = new Random();
 
+  /**
+   * Selects a random category (excluding "NONE")
+   * for mock data generation.
+   *
+   * @return a random category enum that isn't "NONE"
+   */
   public static CategoryEnum getRandomCategory() {
     //return VALUES.get(ThreadLocalRandom.current().nextInt(SIZE)); //could also work?
     return VALUES.get(RANDOM.nextInt(SIZE));
