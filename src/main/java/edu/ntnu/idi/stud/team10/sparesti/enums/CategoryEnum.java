992ff4ff5606ffdfb6c60058da0347996c1ba920
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-22 12:05:07 +0200

    Addition: Add way to get a random category for transaction/budget
    This is a simple method that mainly helps for creating mock data. Implemented in order to generate random mock transactions for a bank account.

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/enums/CategoryEnum.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/enums/CategoryEnum.java
@@ -1,5 +1,11 @@
 package edu.ntnu.idi.stud.team10.sparesti.enums;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+
 /** Enum representing the different categories that a budget row or transaction can have. */
 public enum CategoryEnum {
   GROCERIES,
@@ -7,5 +13,15 @@ public enum CategoryEnum {
   ENTERTAINMENT,
   CLOTHING,
   UTILITIES,
-  OTHER
+  OTHER;
+
+  private static final List<CategoryEnum> VALUES =
+          List.of(values());
+  private static final int SIZE = VALUES.size();
+  private static final Random RANDOM = new Random();
+
+  public static CategoryEnum getRandomCategory() {
+    //return VALUES.get(ThreadLocalRandom.current().nextInt(SIZE)); //could also work?
+    return VALUES.get(RANDOM.nextInt(SIZE));
+  }
 }
