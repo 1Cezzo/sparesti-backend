31aa96692d353539eece86a14f7e59b931c892ed
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-04-23 14:32:47 +0200

    chore: Format

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/dto/TransactionDto.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/dto/TransactionDto.java
@@ -1,11 +1,11 @@
 package edu.ntnu.idi.stud.team10.sparesti.dto;
 
+import java.time.LocalDate;
+
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 /** Dto to represent a transaction entity. */
 @Data
