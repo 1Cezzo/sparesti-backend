c96c0cda6d69a071dedbf15dcd834bf7e0118fb8
SveinKare <sksorestad@gmail.com>
2024-05-01 17:01:59 +0200

    style: spotless

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/service/SavingsGoalService.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/service/SavingsGoalService.java
@@ -1,11 +1,9 @@
 package edu.ntnu.idi.stud.team10.sparesti.service;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
