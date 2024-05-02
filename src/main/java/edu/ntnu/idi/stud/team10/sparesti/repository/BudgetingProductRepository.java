b2c62f8e69795509de61037e3550370af7723141
Svein Kåre <sveiks@stud.ntnu.no>
2024-05-02 18:54:59 +0200

    Fixed broken BudgetingProduct persisting

---
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/repository/BudgetingProductRepository.java
@@ -0,0 +1,7 @@
+package edu.ntnu.idi.stud.team10.sparesti.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import edu.ntnu.idi.stud.team10.sparesti.model.BudgetingProduct;
+
+public interface BudgetingProductRepository extends JpaRepository<BudgetingProduct, Long> {}
