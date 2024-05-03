64d4ad6e8a819cbf517355d6b4b8a1d165ba90c3
SveinKare <sksorestad@gmail.com>
2024-05-03 22:55:58 +0200

    Added file that was removed by accident during merge

---
+++ src/test/java/edu/ntnu/idi/stud/team10/sparesti/SpareStiApplicationTests.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idi.stud.team10.sparesti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpareStiApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
