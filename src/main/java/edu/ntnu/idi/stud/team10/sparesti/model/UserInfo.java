c1b41b455ff21e446c2984d52a8c28f110791501
Svein Kåre <sveiks@stud.ntnu.no>
2024-05-03 14:42:50 +0200

    Formatting

--- src/main/java/edu/ntnu/idi/stud/team10/sparesti/model/UserInfo.java
+++ src/main/java/edu/ntnu/idi/stud/team10/sparesti/model/UserInfo.java
@@ -38,8 +38,7 @@ public class UserInfo {
   @OneToOne
   private User user;
 
-  @Column()
-  private String displayName;
+  @Column() private String displayName;
 
   @Column() private String firstName;
 
