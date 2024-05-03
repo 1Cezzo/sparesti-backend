b28d2da9921bc959a8580b0f3569fdc6fdb0c193
Kristians J. Matrevics <krisjm@stud.ntnu.no>
2024-05-03 21:00:40 +0200

    :art: applied spotless

--- src/test/java/edu/ntnu/idi/stud/team10/sparesti/mapper/AccountMapperTest.java
+++ src/test/java/edu/ntnu/idi/stud/team10/sparesti/mapper/AccountMapperTest.java
@@ -1,64 +1,65 @@
 package edu.ntnu.idi.stud.team10.sparesti.mapper;
 
-import edu.ntnu.idi.stud.team10.sparesti.dto.AccountDto;
-import edu.ntnu.idi.stud.team10.sparesti.model.Account;
+import java.util.HashSet;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.mapstruct.factory.Mappers;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.HashSet;
+import edu.ntnu.idi.stud.team10.sparesti.dto.AccountDto;
+import edu.ntnu.idi.stud.team10.sparesti.model.Account;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AccountMapperTest {
 
-    private AccountMapper accountMapper;
+  private AccountMapper accountMapper;
 
-    @BeforeEach
-    public void setUp() {
-        // Initialize the mapper
-        accountMapper = Mappers.getMapper(AccountMapper.class);
-    }
+  @BeforeEach
+  public void setUp() {
+    // Initialize the mapper
+    accountMapper = Mappers.getMapper(AccountMapper.class);
+  }
 
-    @Test
-    public void shouldMapAccountToAccountDto() {
-        // Given
-        Account account = new Account(1L, 2L, 123456789L, "Savings Account", 1500.0, new HashSet<>());
+  @Test
+  public void shouldMapAccountToAccountDto() {
+    // Given
+    Account account = new Account(1L, 2L, 123456789L, "Savings Account", 1500.0, new HashSet<>());
 
-        // When
-        AccountDto accountDto = accountMapper.toDto(account);
+    // When
+    AccountDto accountDto = accountMapper.toDto(account);
 
-        // Then
-        assertNotNull(accountDto);
-        assertEquals(account.getId(), accountDto.getId());
-        assertEquals(account.getOwnerId(), accountDto.getOwnerId());
-        assertEquals(account.getAccountNr(), accountDto.getAccountNr());
-        assertEquals(account.getName(), accountDto.getName());
-        assertEquals(account.getBalance(), accountDto.getBalance());
-        // Assuming transaction mapping is correctly handled
-        assertNotNull(accountDto.getTransactions());
-    }
+    // Then
+    assertNotNull(accountDto);
+    assertEquals(account.getId(), accountDto.getId());
+    assertEquals(account.getOwnerId(), accountDto.getOwnerId());
+    assertEquals(account.getAccountNr(), accountDto.getAccountNr());
+    assertEquals(account.getName(), accountDto.getName());
+    assertEquals(account.getBalance(), accountDto.getBalance());
+    // Assuming transaction mapping is correctly handled
+    assertNotNull(accountDto.getTransactions());
+  }
 
-    @Test
-    public void shouldMapAccountDtoToAccount() {
-        // Given
-        AccountDto accountDto = new AccountDto(1L, 2L, 123456789L, "Savings Account", 1500.0, new HashSet<>());
+  @Test
+  public void shouldMapAccountDtoToAccount() {
+    // Given
+    AccountDto accountDto =
+        new AccountDto(1L, 2L, 123456789L, "Savings Account", 1500.0, new HashSet<>());
 
-        // When
-        Account account = accountMapper.toEntity(accountDto);
+    // When
+    Account account = accountMapper.toEntity(accountDto);
 
-        // Then
-        assertNotNull(account);
-        assertEquals(accountDto.getId(), account.getId());
-        assertEquals(accountDto.getOwnerId(), account.getOwnerId());
-        assertEquals(accountDto.getAccountNr(), account.getAccountNr());
-        assertEquals(accountDto.getName(), account.getName());
-        assertEquals(accountDto.getBalance(), account.getBalance());
-        // Assuming transaction mapping is correctly handled
-        assertNotNull(account.getTransactions());
-    }
+    // Then
+    assertNotNull(account);
+    assertEquals(accountDto.getId(), account.getId());
+    assertEquals(accountDto.getOwnerId(), account.getOwnerId());
+    assertEquals(accountDto.getAccountNr(), account.getAccountNr());
+    assertEquals(accountDto.getName(), account.getName());
+    assertEquals(accountDto.getBalance(), account.getBalance());
+    // Assuming transaction mapping is correctly handled
+    assertNotNull(account.getTransactions());
+  }
 }
